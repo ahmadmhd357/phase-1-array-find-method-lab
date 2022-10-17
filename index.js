92106d4250147c4e37deae6c86cc4dab686e8a68
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-17 10:40:38 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ];
+function superbowlWin(record){
+    
+   let findYear = record.find(({result})=>result==="W");
+   if(findYear){
+    return findYear.year
+   }
+   else {return undefined}
+}
